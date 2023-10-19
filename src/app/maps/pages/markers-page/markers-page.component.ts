cc3fcf2b3e60b86e2d01f1b1f53a2cf0de74a536
Thomas <thomas.chavezc@utem.cl>
2023-10-19 16:55:47 -0300

    Primer commit

---
+++ src/app/maps/pages/markers-page/markers-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-markers-page',
+  templateUrl: './markers-page.component.html',
+  styleUrls: ['./markers-page.component.css'],
+})
+export class MarkersPageComponent {}
---
+++ src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-zoom-range-page',
+  templateUrl: './zoom-range-page.component.html',
+  styleUrls: ['./zoom-range-page.component.css'],
+})
+export class ZoomRangePageComponent {}
