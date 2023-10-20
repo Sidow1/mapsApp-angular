86714884b6be31b81860a9664ae658d53f5ed88b
Thomas <thomas.chavezc@utem.cl>
2023-10-20 15:45:39 -0300

    Standalone components implementados

--- src/app/maps/maps.module.ts
+++ src/app/maps/maps.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MapsRoutingModule } from './maps-routing.module';
-import { SideMenuComponent } from './components/side-menu/side-menu.component';
+import { SideMenuComponent } from '../alone/components/side-menu/side-menu.component';
 import { MapsLayoutComponent } from './layout/maps-layout/maps-layout.component';
 import { FullScreenPageComponent } from './pages/full-screen-page/full-screen-page.component';
 import { MarkersPageComponent } from './pages/markers-page/markers-page.component';
@@ -11,13 +11,13 @@ import { ZoomRangePageComponent } from './pages/zoom-range-page/zoom-range-page.
 
 import * as mapboxgl from 'mapbox-gl';
 import { MiniMapComponent } from './components/mini-map/mini-map.component';
+import { CounterAloneComponent } from '../alone/components/counter-alone/counter-alone.component';
 
 (mapboxgl as any).accessToken =
   'pk.eyJ1Ijoic2lkb3ciLCJhIjoiY2xueHR0NDFhMGlhYjJscGN2NGtteXZscSJ9.fz445Je8By3wTenFYaTgPw';
 
 @NgModule({
   declarations: [
-    SideMenuComponent,
     MapsLayoutComponent,
     FullScreenPageComponent,
     MarkersPageComponent,
@@ -25,6 +25,11 @@ import { MiniMapComponent } from './components/mini-map/mini-map.component';
     ZoomRangePageComponent,
     MiniMapComponent,
   ],
-  imports: [CommonModule, MapsRoutingModule],
+  imports: [
+    CommonModule,
+    MapsRoutingModule,
+    CounterAloneComponent,
+    SideMenuComponent,
+  ],
 })
 export class MapsModule {}
